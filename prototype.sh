315ced3fe0edba86195fbea9ed2b72dae8f9fca3
root <root@localhost.localdomain>
2019-02-19 23:13:54 +0400

    safeDel prototype v2

--- prototype.sh
+++ prototype.sh
@@ -3,24 +3,19 @@
 #Cpyright (c) Neutron.net
 #This script is for testing purpose only.
 
-echo "=== THIS IS JUST A TESTING ENVIRONMENT ==="
-
-#TEST="Unix Programming"
-
-#echo $PWD
-
-#echo $DISPLAY
-
-#echo $RANDOM
-
-#echo $TEST
-
-#read PERSON
-
-#echo "Hello $PERSON welcome to system programming"
+NAME="Name: Faisal Burhan Abdu"
+STUDENT_ID="STUDENT ID: S1719016"
 
 
-function main(){
+
+#pattern="$1"
+
+#awk '/'"$pattern"'/ { print FILENAME ":" $0 }' "$@"
+
+function main(){	#______________ BEGIN OF MAIN _____________________________
+
+	echo "$NAME"
+	echo "$STUDENT_ID"
 
 	#create a trashCan directory if not exist
 
@@ -30,14 +25,142 @@ function main(){
 
 	if [[ $# == 0 ]];then
 
-		#Display a menu to the user
+		#go interactiive mode.
 
-		display_help_menu
+		init_interactive_mode
+		
+	else
+		if [[ ($1 != -l  && $1 != -r && $1 != -m && $1 != -k && $1 != -d && $1 != -t && $1 != "--help") ]];then
+
+			delete_file $@
+		
+		else
+			
+			#do the command line argument thing ehh.
+
+			init_inlinecmd_mode $@
+		
+		fi
+
+	fi
+	
+	#______________________ END OF MAIN ______________________	
+
+	
+}
+
+
+function init_inlinecmd_mode(){
+
+	#do something...
+	
+	case $1 in
+
+		-l)
+			if [[ $# -gt 1 ]]; then
+				display_help_menu
+			else			
+				#List all files from the trashCan directory"
+				list_files
+			fi
+		;;
+			
+		-r) 
+			if [[ $# -gt 1 ]];then		
+				echo "recover files back to current directory"
+			else
+				display_help_menu
+			fi
+		;;
+
+		-d) 
+			echo "delete content interactively"
+		;;
+		
+		-t) 
+			if [[ $# -gt 1 ]];then
+				display_help_menu
+			else	
+				echo "display total usage in byte of the trashCan directory"	
+			fi
+		;;
+		
+		-m) 
+			if [[ $# -gt 1 ]];then
+				display_help_menu
+			else
+				echo "start monitor process"	
+			fi	
+		;;
+		
+		-k) 
+			if [[ $# -gt 1 ]];then
+				display_help_menu
+			else
+				echo "kill current user's monitor script process"	
+			fi
+		;;
+		
+		--help) 
+			display_help_menu
+		;;
+		
+		esac
+
+
+}
+
+
+function init_interactive_mode(){
+	
+	USAGE="usage: $0 <fill in correct usage>"
+
+	while getopts :lr:dtmk args #options
+	do
+  	case $args in
+		l) echo "l option";;
+     		r) echo "r option; data: $OPTARG";;
+     		d) echo "d option";; 
+     		t) echo "t option";; 
+     		w) echo "m option";; 
+     		k) echo "k option";;     
+     		:) echo "data missing, option -$OPTARG";;
+    		\?) echo "$USAGE";;
+  	esac
+	done
+
+	((pos = OPTIND - 1))
+	shift $pos
+
+	PS3='option> '
+
+	if (( $# == 0 ))
+	then
+		if (( $OPTIND == 1 )) 
+		then
+			select menu_list in list recover delete total watch kill exit
+			do
+				case $menu_list in
+					"list") echo "l";;
+         				"recover") echo "r";;
+         				"delete") echo "d";;
+         				"total") echo "t";;
+         				"monitor") echo "w";;
+         				"kill") echo "k";;
+         				"exit") exit 0;;
+         				*) echo "unknown option";;
+         			esac
+      			done
+ 		fi
+	else
+		echo "extra args??: $@"
 	fi
 
 
-	delete_file $@
+
 }
+
+
 
 function init_trashCan(){
 
@@ -52,19 +175,14 @@ function init_trashCan(){
 
 }
 
-function read_file(){
 
-	while read line; do
-		echo $line
-	done < $1
-
-}
 
 function display_help_menu(){
 	
 	echo "usage: safeDel [OPTION]... [DIRECTORY(S) | FILE(S)]"
 
 }
+
 
 function delete_file(){
 
@@ -99,6 +217,11 @@ function delete_file(){
 			#reset flag back to zero
 			
 			flag=$(( 0 ))
+
+		else
+			echo "File or directory not found, operation cannot be completed!"
+
+			#display_help_menu
 		
 		fi
 
@@ -107,6 +230,71 @@ function delete_file(){
 }
 
 
+#l - list files from .trashCan
+#r - recover files from .trashCan to current working directory
+#k - kill current monitor process
+#d - delete files from .trashCasn directory interactively
+#m - start monitoring script
+#--help - display help to the user on how to use the script
+#t - display total usage of .trashCan directory
+
+
+function list_files(){
+
+	echo "Listing files from trashCan directory..."
+	
+	ls -al ~/.trashCan
+
+
+}
+
+function recover_files(){
+
+	echo "File X will be recovered from .trashCan directory to your current working directory"
+
+	#for TOKEN in $@
+	#do
+	#	file=$TOKEN
+
+	#	if [[ -f $file ]];then
+	#	fi
+
+	#	if [[ -d $file ]];then
+	#	fi
+
+	#	if [[ ]];then
+	#	else
+	#	fi
+	#done
+
+}
+
+
+function kill_monitor_process(){
+
+	echo "Terminating currect running monitor process"
+
+}
+
+function delete_file_intmode(){
+
+	echo "Deleting file X from .trashCan permenently"
+
+}
+
+function init_monitor_sh(){
+
+	echo "Start a new monitor process"
+
+}
+
+
+function display_tc_usage(){
+
+	echo "Display .trashCan total usage..."
+
+}
+
 
 
 #script start execution from main function call below. 
@@ -114,4 +302,6 @@ function delete_file(){
 main $@
 
 
-#END OF safeDel.sh
+#____________________________  END OF safeDel.sh SCRIPT  _________________________________
+
+
